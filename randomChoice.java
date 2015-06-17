0a10f53a99164ff269dc4773d8739a3ca93c2a8d
Grayce Stalowski <grayce.v.stalowski@monmouth.edu>
2015-06-17 15:45:11 -0400

    Switched from array to vector.

--- randomChoice.java
+++ randomChoice.java
@@ -6,18 +6,17 @@ The user enters items one at a time(can be multiple words) and types "end" to fi
 The program picks one of the items and displays it to the user.
 */
 import java.util.Scanner;
+import java.util.*;
 public class randomChoice
 {
   public static void main(String[]args)
   {
-    int size=100; //Change this number to change the max number of choices someone can enter
-    int x=0; //Variable to keep track of the array position
 
-    String[] choices = new String[size]; //The array
+    Vector choices = new Vector(2,1); //Vector that has an initial capacity of 2 and then increases by 1 when it is reached
     System.out.println("Welcome to Grayce's Chooser");
-    System.out.println("Enter up to " + size + " choices, hitting enter between each one");
+    System.out.println("Enter choices, hitting enter between each one");
     System.out.println("Type \"end\" when you are finished");
-   
+    
     Scanner scan = new Scanner(System.in);
     String input=scan.nextLine(); //Get first user input
     
@@ -28,17 +27,17 @@ public class randomChoice
        input=scan.nextLine();
     }
      
-    //Adds the users input to the array until "end" is typed
+    //Adds the users input to the vector until "end" is typed
+    //Vectors are dynamic and work a lot better than arrays since here we do not know how much input to expect
     while(!input.equalsIgnoreCase("end"))
     {
-      choices[x]=input;
+      choices.add(input);
       input=scan.nextLine();
-      x++;
     }
     
-    //Makes a random number, multiplies it by the size of the array, and makes it an int to be used to retrieve the value at that array position
-    int ranNum= (int)(Math.random() * x);
-    System.out.println("You should choose \"" + choices[ranNum]+"\"");
+    //Makes a random number, multiplies it by the size of the vector, and makes it an int to be used to retrieve the value at that position
+    int ranNum= (int)(Math.random() * choices.size());
+    System.out.println("You should choose \"" + choices.elementAt(ranNum)+"\"");
     
   }//end main
 }//end class
